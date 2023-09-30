8f6bc492df3dfa0a29ed797e74512610c8a33b91
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2023-09-30 19:07:45 +0500

    Sending Data from Controller to View using Model

--- src/main/java/com/spring_mvc/controller/HomeController.java
+++ src/main/java/com/spring_mvc/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.spring_mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,7 +9,9 @@ public class HomeController {
 
 	
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("name","Asad");
+		model.addAttribute("rollNo" , 62);
 		return "home";
 	}
 	@RequestMapping("/login")
--- src/main/webapp/WEB-INF/view/home.jsp
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -7,7 +7,12 @@
 <title>Insert title here</title>
 </head>
 <body>
+	<% String name = (String) request.getAttribute("name"); 
+	   int rollNo = (Integer) request.getAttribute("rollNo");
+	%>
 	<h1>Hello from Home Jsp</h1>
+	<h1>Name :  <%= name %></h1>
+	<h1>RollNo : <%= rollNo %></h1>
 	<a href="login">Login</a>
 </body>
 </html>
